f212e31fcda49406f229476d5979cf3aec7fdfcb
magarcia <newluxfero@gmail.com>
2013-02-22 13:24:35 +0100

    Initial client code

---
+++ S1/client.erl
@@ -0,0 +1,47 @@
+-module(client).
+
+%% Exported Functions
+-export([start/2, init_client/2]).
+
+%% API Functions
+start(ServerPid, MyName) ->
+    ClientPid = spawn(client, init_client, [ServerPid, MyName]),
+    process_commands(ServerPid, MyName, ClientPid).
+
+init_client(ServerPid, MyName) ->
+    ServerPid ! {client_join_req, ..., ...},  %% TODO: COMPLETE
+    process_requests().
+
+%% Local Functions
+%% This is the background task logic
+process_requests() ->
+    receive
+        {join, Name} ->
+            io:format("[JOIN] ~s joined the chat~n", [Name]),
+            %% TODO: ADD SOME CODE
+
+        {leave, Name} ->
+            %% TODO: ADD SOME CODE
+            process_requests();
+
+        {message, Name, Text} ->
+            io:format("[~s] ~s", [Name, Text]),
+            %% TODO: ADD SOME CODE
+
+        exit ->
+            ok
+    end.
+
+%% This is the main task logic
+process_commands(ServerPid, MyName, ClientPid) ->
+    %% Read from standard input and send to server
+    Text = io:get_line("-> "),
+    if
+        Text  == "exit\n" ->
+            ServerPid ! {client_leave_req, ..., ...},  %% TODO: COMPLETE
+            ok;
+
+        true ->
+            ServerPid ! {send, MyName, Text},
+            process_commands(ServerPid, MyName, ClientPid)
+end.
