26a45b7122be994bbf8b210c5818ea559778531f
magarcia <newluxfero@gmail.com>
2013-02-22 13:04:01 +0100

    Initial server code

---
+++ S1/server.erl
@@ -0,0 +1,35 @@
+-module(server).
+
+%% Exported Functions
+-export([start/0, process_requests/1]).
+
+%% API Functions
+start() ->
+    ServerPid = spawn(server, process_requests, [ [] ]),
+    register(myserver, ServerPid).
+
+process_requests(Clients) ->
+    receive
+        {client_join_req, Name, From} ->
+            NewClients = [...|Clients],  %% TODO: COMPLETE
+            broadcast(NewClients, {join, Name}),
+            process_requests(...);  %% TODO: COMPLETE
+
+        {client_leave_req, Name, From} ->
+            NewClients = lists:delete(..., Clients),  %% TODO: COMPLETE
+            broadcast(Clients, ...),  %% TODO: COMPLETE
+            From ! exit,
+            process_requests(...);  %% TODO: COMPLETE
+
+        {send, Name, Text} ->
+            broadcast(..., ...),  %% TODO: COMPLETE
+            process_requests(Clients);
+
+        disconnect ->
+            unregister(myserver)
+end.
+
+%% Local Functions
+broadcast(PeerList, Message) ->
+    Fun = fun(Peer) -> Peer ! Message end,
+    lists:map(Fun, PeerList).
