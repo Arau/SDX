c0d4844ba87d0b1e3715c333c3885293fde2161b
Ferran Arau <ferran.arau.castell@gmail.com>
2013-04-19 13:30:26 +0200

    Cleaning code

--- S4/groupy.erl
+++ S4/groupy.erl
@@ -2,11 +2,12 @@
 -export([start/2, stop/0]).
 
 start(Module, Sleep) ->
-register(a, Leader),
-register(b, worker:start("2", Module, 2, Leader, Sleep)),
-register(c, worker:start("3", Module, 3, Leader, Sleep)),
-register(d, worker:start("4", Module, 4, Leader, Sleep)),
-register(e, worker:start("5", Module, 5, Leader, Sleep)).
+    Leader = worker:start("1", Module, 1, Sleep),
+    register(a, Leader),
+    register(b, worker:start("2", Module, 2, Leader, Sleep)),
+    register(c, worker:start("3", Module, 3, Leader, Sleep)),
+    register(d, worker:start("4", Module, 4, Leader, Sleep)),
+    register(e, worker:start("5", Module, 5, Leader, Sleep)).
 
 stop() ->
     a ! stop,
