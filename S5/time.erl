c896a559b663895092a1a89dfcb4f662cbc266d5
Ferran Arau <ferran.arau.castell@gmail.com>
2013-05-09 11:39:32 +0200

    Change tabs to spaces

--- S5/time.erl
+++ S5/time.erl
@@ -2,14 +2,14 @@
 -export([now/0, add/2, inf/0, valid/2]).
 
 now() ->
-	{H, M, S} = erlang:time(),
-	H*3600+M*60+S.
+    {H, M, S} = erlang:time(),
+    H*3600+M*60+S.
 
-inf() ->
-	inf.
+inf() 
+    inf.
 
 add(S, T) ->
-	S + T.
+    S + T.
 
 valid(C, T) ->
-	C > T.
+    C > T.
