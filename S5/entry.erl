b425b19f745b5461c8f3cd5c060f5523945209f0
Ferran Arau <ferran.arau.castell@gmail.com>
2013-05-10 13:33:14 +0200

    Lookup in List of tuples

---
+++ S5/entry.erl
@@ -0,0 +1,16 @@
+-module(entry).
+-export([lookup/2, add/3, remove/2]).
+
+lookup(Req, Entries) ->
+    Tuple = lists:keyfind(Req, 1, Entries),
+    if
+        not Tuple ->
+            unknown;
+
+        true ->
+            Tuple
+    end.
+
+%% add(Name, Entry, Entries) ->
+
+%% remove(Name, Entries) ->
