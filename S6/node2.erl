eb9dba7f8b118766a04479692218303d480d72b1
Ferran Arau <ferran.arau.castell@gmail.com>
2013-06-04 10:19:51 +0200

    Node 2: Implementation with key store

---
+++ S6/node2.erl
@@ -0,0 +1,149 @@
+-module(node2).
+-export([node/4, start/1, start/2]).
+
+start(MyKey) ->
+    start(MyKey, nil).
+
+start(MyKey, PeerPid) ->
+    timer:start(),
+    spawn(fun() -> init(MyKey, PeerPid) end).
+
+init(MyKey, PeerPid) ->
+    Predecessor = nil,
+    {ok, Successor} = connect(MyKey, PeerPid),
+    schedule_stabilize(),
+    node(MyKey, Predecessor, Successor, []).
+
+-define(Timeout, 5000).
+
+connect(MyKey, nil) ->
+    {ok, {MyKey, self()}};                                  %% I'm the only node in ring
+
+connect(_, PeerPid) ->
+    Qref = make_ref(),
+    PeerPid ! {key, Qref, self()},
+    receive
+        {Qref, Skey} ->
+            {ok, {Skey, PeerPid}}                           %% Succesor replies
+
+    after ?Timeout ->
+        io:format("Timeout: no response from ~w~n", [PeerPid])
+
+    end.
+
+-define(Stabilize, 1000).
+
+schedule_stabilize() ->
+    timer:send_interval(?Stabilize, self(), stabilize).
+
+stabilize({_, Spid}) ->
+    Spid ! {request, self()}.
+
+stabilize(Pred, MyKey, Successor) ->
+    {Skey, Spid} = Successor,
+    case Pred of
+        nil ->
+            Spid ! {notify,{MyKey, self()}},                 %% Send notify msg to my new succesor
+            Successor;
+        {MyKey, _} ->
+            Successor;
+        {Skey, _} ->
+            Spid ! {notify, {MyKey, self()}},                %% Send notify msg to my new succesor
+            Successor;
+        {Xkey, Xpid} ->
+            case key:between(Xkey, MyKey, Skey) of
+                true ->
+                    Xpid ! {request, self()},                %% Pred. is my new Successor
+                    Pred;                                    %% Stabilize my own
+                false ->
+                    Spid ! {notify, {MyKey, self()}},        %% Send notify msg to my new succesor
+                    Successor
+            end
+    end.
+
+node(MyKey, Predecessor, Successor, Store) ->
+    receive
+        {key, Qref, PeerPid} ->
+            PeerPid ! {Qref, MyKey},
+            node(MyKey, Predecessor, Successor, Store);
+
+        {notify, New} ->
+            Pred = notify(New, MyKey, Predecessor),
+            node(MyKey, Pred, Successor, Store);
+
+        {request, Peer} ->
+            request(Peer, Predecessor),
+            node(MyKey, Predecessor, Successor, Store);
+
+        {status, Pred} ->
+            Succ = stabilize(Pred, MyKey, Successor),
+            node(MyKey, Predecessor, Succ, Store);
+
+        stabilize ->
+            stabilize(Successor),
+            node(MyKey, Predecessor, Successor, Store);
+
+        probe ->
+            create_probe(MyKey, Successor),
+            node(MyKey, Predecessor, Successor, Store);
+
+        {probe, MyKey, Nodes, T} ->
+            remove_probe(MyKey, Nodes, T),
+            node(MyKey, Predecessor, Successor, Store);
+
+        {probe, RefKey, Nodes, T} ->
+            forward_probe(RefKey, [MyKey | Nodes], T, Successor),
+            node(MyKey, Predecessor, Successor, Store);
+
+       {add, Key, Value, Qref, Client} ->
+            Added = add(Key, Value, Qref, Client,
+                        MyKey, Predecessor, Successor, Store),
+            node(MyKey, Predecessor, Successor, Added, Store);
+
+        {lookup, Key, Qref, Client} ->
+            lookup(Key, Qref, Client, MyKey, Predecessor, Successor, Store),
+            node(MyKey, Predecessor, Successor, Store)
+    end.
+
+request(Peer, Predecessor) ->
+    case Predecessor of
+        nil ->
+            Peer ! {status, nil};
+        {Pkey, Ppid} ->
+            Peer ! {status, {Pkey, Ppid}}
+    end.
+
+
+notify({Nkey, Npid}, MyKey, Predecessor) ->
+    case Predecessor of
+        nil ->
+            {Nkey, Npid};                           %% I'm new predecessor
+
+        {Pkey, _} ->
+            case key:between(Nkey, Pkey, MyKey) of
+                true ->
+                    {Nkey, Npid};                   %% I'm new predecessor
+                false ->
+                    Predecessor
+        end
+    end.
+
+create_probe(MyKey, {_, Spid}) ->
+    Spid ! {probe, MyKey, [MyKey], erlang:now()},
+    io:format("Create probe ~w!~n", [MyKey]).
+
+remove_probe(MyKey, Nodes, T) ->
+    Time = timer:now_diff(erlang:now(), T),
+    io:format("Received probe ~w in ~w ms Ring: ~w~n", [MyKey, Time, Nodes]).
+
+forward_probe(RefKey, Nodes, T, {_, Spid}) ->
+    Spid ! {probe, RefKey, Nodes, T},
+    io:format("Forward probe ~w!~n", [RefKey]).
+
+add(Key, Value, Qref, Client, MyKey, {Pkey, _}, {_, Spid}, Store) ->
+    end.
+
+lookup(Key, Qref, Client, MyKey, {Pkey, _}, {_, Spid}, Store) ->
+    end.
+
+
